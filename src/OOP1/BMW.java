d2494c7954c087282fd7d1b61bb0dd77f4b6f223
gitRaj <rajendra.sahoo.meet@gmail.com>
2024-07-19 18:38:30 +0530

    added broom in bmw.java line 8

--- src/OOP1/BMW.java
+++ src/OOP1/BMW.java
@@ -5,7 +5,7 @@ public class BMW extends Car{
 	
 	//Method Overriding: when the same method is present in parent class as well as in child class with the same and same no. of parameters.
 	public void start(){
-		System.out.println("BMW--start");
+		System.out.println("BMW--start brommmm");
 	}
 	
 	
